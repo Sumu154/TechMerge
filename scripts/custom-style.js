9f0f35075e150a09e2b39963d0a560d2f496465f
Sumu154 <sumaiyatasnim5548@gmail.com>
2024-10-04 13:00:40 +0600

    commit

---
+++ scripts/custom-style.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        fontFamily: {
+          'poppins': ['Poppins', 'sans-serif'],
+        },
+        
+        colors: {
+          textcolor: '#20204E',
+          purple: '#424874',
+          lightpurple: '#5F6FB5',
+          skyblue: '#A6B1E1',
+          lightblue: '#DCD6F7',
+          extralight: '#F4EEFF',
+          black: '#000000',
+          white: '#FFFFFF',
+          orange: '#FA7635',
+          bordercolor: 'rgba(17, 17, 17, 0.30)',
+        },
+
+        backgroundImage: {
+          'bginput': "url('assets/image/bg.jpg')",
+        }
+      }
+    }
+  }
